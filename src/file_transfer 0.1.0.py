59e761203c0ad5d8369dc79166bff28daa45f4a8
AlegriaPonceJoseSantiago343 <zs24013141@estudiantes.uv.mx>
2026-04-24 18:55:29 -0600

    Update file_transfer 0.2.0-Santiago.py
    Añadí la conexión de computadoras, ahora dos dispositivos pueden hablar entre sí pero sin enviar archivo, solo mensajes de pruebas.

--- src/file_transfer 0.1.0.py
+++ src/file_transfer 0.1.0.py
@@ -1,29 +1,40 @@
-# Autor: Alfredo
-# fecha:24/04/2026
+#Autor:Alfredo
+#Modificador: Jose santiago
+#Fecha:24/04/26
+
+import socket
 import argparse
 
 def recibir_archivo(host, port):
-    print(f"[*] Modo Servidor: Preparado para recibir en {host}:{port}")
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((host, port))
+    server.listen(1)
+    print(f"[*] Esperando archivo en {host}:{port}...")
+    
+    conn, addr = server.accept()
+    with conn:
+        print(f"[+] Conexión de {addr} para transferencia.")
+        mensaje = conn.recv(1024).decode('utf-8')
+        print(f"[*] Mensaje recibido: {mensaje}")
+    server.close()
 
 def enviar_archivo(host, port, ruta_archivo):
-    print(f"[*] Modo Cliente: Preparado para enviar '{ruta_archivo}' a {host}:{port}")
+    cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    cliente.connect((host, port))
+    cliente.sendall(b'Hola, estoy listo para enviar el archivo')
+    print("[*] Conexion exitosa y mensaje enviado.")
+    cliente.close()
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Transferencia de Archivos")
+    parser = argparse.ArgumentParser()
     parser.add_argument('--mode', choices=['receive', 'send'], required=True)
     parser.add_argument('--host', required=True)
     parser.add_argument('--port', type=int, default=5001)
     parser.add_argument('--file')
-    
     args = parser.parse_args()
     
-    if args.mode == 'receive':
-        recibir_archivo(args.host, args.port)
-    elif args.mode == 'send':
-        if not args.file:
-            print("[!] Debes especificar el archivo con --file")
-        else:
-            enviar_archivo(args.host, args.port, args.file)
-            # Esto es lo que se pone en la terminal CMD
-            # git add src/file_transfer.py
-# git commit -m "feat: crear estructura inicial y argparse para transferencia de archivos — [Nombre del Integrante 4]"
+    if args.mode == 'receive': recibir_archivo(args.host, args.port)
+    elif args.mode == 'send': enviar_archivo(args.host, args.port, args.file)
+
+#En el CMD se pone esto para que se ejecute:
+#git add src/file_transfer.py
